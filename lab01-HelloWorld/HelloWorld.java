39914dcd3b5b6fbd6d0b4acddde9a40218f9ab2e
aebersole3 <aje215@lehigh.edu>
2015-01-23 16:17:35 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////////////////////
+//Alan Ebersole
+//lab01
+//Hello World Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+//  define a class
+public class HelloWorld{
+
+//  add main method
+    public static void main(String[] args) {
+    
+//  print main method
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2015!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("testing");
+    
+    //test/testtest
+    }
+}
