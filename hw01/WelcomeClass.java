738089f3de6c655c5e80b480055e8bfc099e32a2
aebersole3 <aje215@lehigh.edu>
2015-01-26 18:47:52 +0000

    update WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////////////////
+//Alan Ebersole
+//hw01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass
+//
+//  define a class
+public class WelcomeClass{
+    
+//add main method
+    public static void main(String[] args) {
+    
+//  print main method
+        System.out.println("  -----------");
+        System.out.println("  | Welcome |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-A--J--E--2--1--5->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v ");
+    }
+}
