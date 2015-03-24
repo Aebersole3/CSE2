3973c1f4afa0b810f094fc4d02ceeccdc9b598f1
aebersole3 <aje215@lehigh.edu>
2015-03-24 01:15:11 +0000

    HW8 upload

--- hw08/HW8.java
+++ hw08/HW8.java
@@ -9,53 +9,58 @@ public class HW8{
     
     public static String getInput(Scanner scan, String string){
         String scanner="";
-        scanner= scanner+ scan.next().trim().charAt(0);
-        if(scanner.equals("C") || scanner.equals("c") ){
+        scanner= scanner+ scan.next().trim().charAt(0);//gets the character of the input
+        if(scanner.equals("C") || scanner.equals("c") ){//tests for user input
             return scanner;
         }
         else{
             System.out.println("Yea right LOSER!");
-            System.exit(0);
+            System.exit(0);//exits the program
             return scanner;
         }
         
     }
     
     public static String getInput(Scanner scan, String string, int trial){
+            //declare variables
             int rollForCrit;
             int rollToEscape;
-            while(trial>0){
+            
+            while(trial>0){//while loop for fighting giant
                 char turnAttack =scan.next().trim().charAt(0);
                 switch(turnAttack){
                     case 'A':
                     case 'a':
-                        rollForCrit= (int)(Math.random()+.1);
+                        rollForCrit= (int)(Math.random()+.1);//roll for critical hit 0-1
                         switch(rollForCrit){
-                            case 1:
+                            case 1://if a 1 is rolled, they crit
                                 System.out.println("Critical Hit!");
-                                trial--;
+                                trial--; //treat trial like hp, so each hit decriments it
                                 break;
                             case 0:
                                 System.out.println("Gosh! How can you miss it!");
                                 break;
                         }
                         break;
+                    //case if the try to run
                     case 'E':
                     case 'e':
                         rollToEscape= (int)((10*Math.random())+1);
-                        if (rollToEscape==10){
+                        if (rollToEscape==10){//if they succeed the roll for running
                             System.out.println("You have escaped the Giant!");
+                            System.exit(0);//exits the program
                         }
                         else{
                             System.out.println("You failed to escape the Giant!");
                             return "";
                         }
                         break;
-                    default:
+                    default://default case that ends the program
                     System.out.println("Executed by the GIANT! Game Over! ");
                     System.exit(0);
                         break;
                 }
+                //if statement to print out the prompt to do a move unless the giant is at zero hp(trial==0)
                 if(trial==0){
                 return "";
                 }
@@ -67,6 +72,7 @@ public class HW8{
         return "";
     }
     
+    //simple method that uses a switch statement to prin out the prize or exit the program
     public static String getInput(Scanner scan){
         char chestPick =scan.next().trim().charAt(0);
         switch(chestPick){
